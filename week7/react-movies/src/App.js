8652053a265de8a69ce17ee026ec0a1f219bc989
yuzhouw313 <yuzhouw@uchicago.edu>
2024-05-01 16:43:06 -0500

    finish week

--- week7/react-movies/src/App.js
+++ week7/react-movies/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// import React, { useState } from 'react';
+import { React, useState } from 'react';
 
 function Movie(props) {
   return (
@@ -10,32 +11,44 @@ function Movie(props) {
         <span className="badge bg-primary mx-3">
           {props.vote_average.toFixed(1)}
         </span>
-        <LikeButton/>
+        <LikeButton clicked={props.liked} counter={props.likes} key={props.title}/>
       </p>
     </div>
   )
 }
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { counter: 0 }
+function LikeButton(props) {
+  
+  //useState a function from React and return an array, first element is state 
+  // the second is a function to change the state, input 0 is the default, then will be updated
+  // const [counter, setCounter]  = useState(0) 
+
+  function handleClick(event) {
+    event.preventDefault()
+    props.clicked({ props.title: props.couner + 1})
+    // setCounter(counter + 1)
   }
 
-  handleClick = (event) => {
-    console.log(event)
-    this.setState({ counter: this.state.counter + 1 })
-  }
 
-  render() {
     return (
-      <button onClick={this.handleClick} className="text-decoration-none btn text-danger ">&hearts; <span>{this.state.counter}</span></button>
+      <button onClick={handleClick} className="text-decoration-none btn text-danger ">&hearts; <span>{props.counter}</span></button>
     )
-  }
 }
 
 function App() {
-  const data = [
+
+  function handleTopRated(props) {
+    const url = urlForMovies("top_rated")
+    fetch(url).then((r) => r.json()).then((internet_data) => setData(internet_data.results))
+  }
+
+  function handleNowPlaying(props){
+    const url = urlForMovies("now_playing")
+    fetch(url).then((r) => r.json()).then((internet_data) => setData(internet_data.results))
+  }
+
+
+  const initialData = [
     { title: "The Princess Bride", poster_path: '/dvjqlp2sAhUeFjUOfQDgqwpphHj.jpg', release_date: '1999', vote_average: 10 },
     { title: "Spider - Man", poster_path: '/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg', release_date: '1999', vote_average: 10 },
     { title: "Star Wars", poster_path: '/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg', release_date: '1999', vote_average: 10 },
@@ -45,8 +58,11 @@ function App() {
     { title: "The Matrix", poster_path: '//f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg', release_date: '1999', vote_average: 10 },
     { title: "Toy Story", poster_path: '/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg', release_date: '1999', vote_average: 10 },
   ]
-  
-  const movies = data.map(movie_data => <Movie title={movie_data.title} release_date={movie_data.release_date} poster_path={movie_data.poster_path} vote_average={movie_data.vote_average} />)
+
+  const [likes, setLikes] = useState( {} )
+  const [data, setData] = useState(initialData)
+
+  const movies = data.map(movie_data => <Movie liked={setLikes} likes={likes[movie_data.title] || 0} key={movie_data.title} title={movie_data.title} release_date={movie_data.release_date} poster_path={movie_data.poster_path} vote_average={movie_data.vote_average} />)
 
   return (
     <div>
@@ -57,8 +73,8 @@ function App() {
         </form>
 
         <p className="mt-2">
-          <button className="btn btn-primary" >Top-Rated Movies</button>
-          <button className="btn btn-primary ms-3">Now Playing</button>
+          <button onClick={handleTopRated} className="btn btn-primary" >Top-Rated Movies</button>
+          <button onClick={handleNowPlaying} className="btn btn-primary ms-3">Now Playing</button>
         </p>
       </header>
 
